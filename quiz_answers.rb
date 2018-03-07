aab38f51501b310df3ce79277eba8caaa454fe67
BKertesz <kertesz.balazs92@gmail.com>
2018-03-07 20:28:45 +0000

    Finished version of quiz

--- quiz_answers.rb
+++ quiz_answers.rb
@@ -49,31 +49,63 @@ users = {
   },
 }
 # 1.Answer
-
+p users["Jonathan"][:twitter]
 # 2.Answer
-
+p users["Erik"][:home_town]
 # 3.Answer
-
+p users["Erik"][:favourite_numbers]
 # 4.Answer
-
+p users["Avril"][:pets]["colin"]
 # 5.Answer
-
+p users["Erik"][:favourite_numbers].min
 # 6.Answer
-
+users["Erik"][:favourite_numbers].push(7)
 # 7.Answer
-
+users["Erik"][:home_town] = 'Edinburgh'
 # 8.Answer
-
+users["Erik"][:pets]['Fluffy'] = :dog
 # 9.Answer
-
+users["Balazs"] = {}
+users["Balazs"][:home_town] = "Pitlochry"
 # End of second third of quiz
 
 
 # Begining of third third of quiz
+united_kingdom = [
+  {
+    name: "Scotland",
+    population: 5295000,
+    capital: "Edinburgh"
+  }, {
+    name: "Wales",
+    population: 3063000,
+    capital: "Swansea"
+  }, {
+    name: "England",
+    population: 53010000,
+    capital: "London"
+  }
+]
 
-
-
-
+# 1.Answer
+united_kingdom[1][:capital] = "Cardiff"
+# 2.Answer
+united_kingdom[3] = {}
+united_kingdom[3][:name] = 'Northern Ireland'
+united_kingdom[3][:population] =1,811,000
+united_kingdom[3][:capital]= "Belfast"
+# 3.Answer
+p "Parts of the United Kingdom"
+for i in united_kingdom
+  p "-#{i[:name]}"
+end
+# 4.Answer
+total_population = 0
+for i in united_kingdom
+  # total_population += i[:population]
+  total_population += i[:population].to_s.to_i
+end
+p "Total population is #{total_population}"
 
 # End of third third of quiz
 
