c510894d8eed3d4e6e5e73d056ae393e3b65a732
BKertesz <kertesz.balazs92@gmail.com>
2018-03-07 20:08:12 +0000

    Added formating to answers

--- quiz_answers.rb
+++ quiz_answers.rb
@@ -1,14 +1,81 @@
+# Begining of first third of quiz
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-stops.push("Edinburgh Waverley")
-stops.unshift("Glasgow Queen St")
-stops.insert(4,"Polmont")
-p stops[5]
-stops.delete("Livingston")
-stops.delete_at(2)
-p "Stops on the train: #{stops.length()}"
-falkirk_high = stops[2]
-stops.reverse!
+stops.push("Edinburgh Waverley") #1.Answer
+stops.unshift("Glasgow Queen St")#2.Answer
+stops.insert(4,"Polmont")#3.Answer
+p stops[5] #4.Answer
+stops.delete("Livingston") #5.Answer
+stops.delete_at(2) #6.Answer
+p "Stops on the train: #{stops.length()}" #7.Answer
+falkirk_high = stops[2] #8.Answer
+stops.reverse! #9.Answer
 
-for i in stops
+for i in stops #10.Answer
   p "Next stop is:#{i}"
 end
+# End of first third of quiz
+
+
+# Beining of second third of quiz
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :favourite_numbers => [12, 42, 75, 12, 5],
+    :home_town => "Stirling",
+    :pets => {
+      "fluffy" => :cat,
+      "fido" => :dog,
+      "spike" => :dog
+    }
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :favourite_numbers => [8, 12, 24],
+    :home_town => "Linlithgow",
+    :pets => {
+      "nemo" => :fish,
+      "kevin" => :fish,
+      "spike" => :dog,
+      "rupert" => :parrot
+    }
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :favourite_numbers => [12, 14, 85, 88],
+    :home_town => "Dunbar",
+    :pets => {
+      "colin" => :snake
+    }
+  },
+}
+# 1.Answer
+
+# 2.Answer
+
+# 3.Answer
+
+# 4.Answer
+
+# 5.Answer
+
+# 6.Answer
+
+# 7.Answer
+
+# 8.Answer
+
+# 9.Answer
+
+# End of second third of quiz
+
+
+# Begining of third third of quiz
+
+
+
+
+
+# End of third third of quiz
+
+
+# End of quiz
